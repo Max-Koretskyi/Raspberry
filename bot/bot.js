c8b9544649354e9647cd391b7d561d69bb894a11
Max-Koretskyi <happy.butchers.m@gmail.com>
2020-10-18 01:48:59 +0300

    test

---
+++ bot/bot.js
@@ -0,0 +1,11 @@
+const BOT_TOKEN = "1042353212:AAGrFdM1MFRpOlJL1sFenXqvO0gfyT0Asmk"
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf(BOT_TOKEN, {polling:true})
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker',(ctx) => ctx.reply('wow!!!'))
+bot.hears('Hi', (ctx) => ctx.reply('Hi there'))
+
+
+bot.launch()
